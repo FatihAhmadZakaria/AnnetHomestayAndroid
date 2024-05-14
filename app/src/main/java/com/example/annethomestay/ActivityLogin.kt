96dee1ad49328ef248d18b01acf98e7f316d83cb
SAYA <fatihahmadzakaria@gmail.com>
2024-05-14 22:39:53 +0700

    On progress 1

---
+++ app/src/main/java/com/example/annethomestay/ActivityLogin.kt
@@ -0,0 +1,33 @@
+package com.example.annethomestay
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.annethomestay.databinding.ActivityLoginBinding
+import android.view.animation.AnimationUtils
+
+class ActivityLogin : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val anim = AnimationUtils.loadAnimation(this@ActivityLogin, R.anim.bt_anim)
+
+        binding.btLogin.setOnClickListener {
+            it.startAnimation(anim)
+            Toast.makeText(this@ActivityLogin, "Button clicked", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
